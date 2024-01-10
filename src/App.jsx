da826f5b9e707569522474874d41d28a2bb87939
Soab42 <108150913+Soab42@users.noreply.github.com>
2024-01-10 08:45:49 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center text-[5rem] bg-slate-900 h-screen  text-sky-600">
+        Welcome! Start Your Coding From Here.
+      </div>
+    </>
+  );
+}
+
+export default App;
